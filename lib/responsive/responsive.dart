43c999432fe4a1484f0f5c59b2e6bdc4e6b5f476
Jn-Sajol <julkar10121@gmail.com>
2024-02-25 21:04:24 +0600

    make responsive weidget

---
+++ lib/responsive/responsive.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class Responsive extends StatelessWidget {
+final Widget mobilescreen;
+final Widget webscreen;
+  const Responsive({super.key, required this.mobilescreen, required this.webscreen});
+ final int screenSize = 600;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+    builder:(context,Constraints){
+    if(Constraints.minWidth>screenSize){
+    return webscreen;
+    }else {
+    return mobilescreen;
+    }
+    });
+  }
+}
---
+++ lib/responsive/web_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class WebScreen extends StatelessWidget {
+  const WebScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    body: Text("web page"),
+    );
+  }
+}
